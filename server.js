f91ac123b60f90f884f697d6a69dd2d89dc9c714
shamim 1845m <samimraj1845@gmail.com>
2022-12-01 08:53:30 +0600

    cors origin added

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://inventory-management-app-backend.onrender.com"],
     credentials: true
 }))
 
